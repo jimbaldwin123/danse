dc5266e42288f76cd7931eea141f44aac3eea69d
danse <danse@coffee.dreamhost.com>
2018-11-29 06:19:10 -0800

    remove some old abwd folder

--- max/divisions/subnav/leftnav_test.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-
-// Names of Sub Nav Links as seen to the user
-// The order matters, so arrange accordingly
-// Make sure you relative or absolutely link the items
-
-$link_labels = array(	"MaxOnline" => "http://www.maxonline.com",
-						"MaxCreative" => "http://creative.maxworldwide.com");
-?>
